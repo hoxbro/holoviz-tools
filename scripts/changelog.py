3c3b0c6b69e9495fa4a6f6856ea57814a2038391
Simon Høxbro Hansen <simon.hansen@me.com>
2023-05-16 17:23:06 +0200

    Update changelog

--- scripts/changelog.py
+++ scripts/changelog.py
@@ -1,4 +1,6 @@
 import re
+from textwrap import dedent
+
 from pandas.io.clipboard import clipboard_get, clipboard_set
 
 # https://github.com/holoviz/holoviews/releases/new
@@ -7,14 +9,16 @@ from pandas.io.clipboard import clipboard_get, clipboard_set
 def main() -> None:
     cl = clipboard_get()
 
-    commits = re.findall("\* (.+?)\.? by (@.+?) in (.+?)\n", cl)
+    commits = re.findall("\\* (.+?)\\.? by (@.+?) in (.+?)\n", cl)
     users, msgs = set(), ""
     for c in commits:
         no = c[2].split("/")[-1]
-        msgs += f"- {c[0].strip().capitalize()} ([#{no}]({c[2]}))\n"
+        msg = c[0].strip()
+        msg = msg[0].upper() + msg[1:]
+        msgs += f"- {msg} ([#{no}]({c[2]}))\n"
         users |= {c[1]}
 
-    new_users = set(re.findall("\* (@.+?) ", cl))
+    new_users = set(re.findall("\\* (@.+?) ", cl))
     users = users - new_users
 
     template = f"""
@@ -28,13 +32,12 @@ def main() -> None:
     Maintenance:
 
     (Not sorted commits)
-
-    {msgs}
     """
 
-    clipboard_set(template)
+    output = dedent(template) + msgs
 
-    print(template)
+    clipboard_set(output)
+    print(output)
 
 
 if __name__ == "__main__":
