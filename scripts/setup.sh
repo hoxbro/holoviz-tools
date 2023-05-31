ad908f646a3246b62643b5e0fee6c3a89ae8652d
Simon Høxbro Hansen <simon.hansen@me.com>
2023-05-31 11:09:36 +0200

    Add conda env PYDEVD_DISABLE_FILE_VALIDATION

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -112,6 +112,9 @@ create_environments() {
         conda env config vars set BOKEH_PRETTY=true -n $CONDA_ENV
         conda env config vars set SETUPTOOLS_ENABLE_FEATURES=legacy-editable -n $CONDA_ENV
         conda env config vars set USE_PYGEOS=0 -n $CONDA_ENV
+
+        # https://discourse.jupyter.org/t/debugger-warning-it-seems-that-frozen-modules-are-being-used-python-3-11-0
+        conda env config vars set  PYDEVD_DISABLE_FILE_VALIDATION=1 -n $CONDA_ENV
         # Crashes pytest https://github.com/microsoft/vscode-python/issues/20259
         # conda env config vars set PYTHONWARNINGS=default
 
