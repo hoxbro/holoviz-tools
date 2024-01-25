36fe2d89b7ed9d7cbffd4b136275ec79e6c67309
Simon Høxbro Hansen <simon.hansen@me.com>
2024-01-25 08:56:54 +0100

    Show all deprecated functions

--- scripts/deprecate.py
+++ scripts/deprecate.py
@@ -9,23 +9,30 @@ from packaging.version import Version
 CUR_VERSION = Version(pn.__version__)
 BASE_CUR_VERSION = Version(CUR_VERSION.base_version)
 PATH = os.path.dirname(pn.__file__)
+GREEN, RED, RESET = "\033[92m", "\033[91m", "\033[0m"
 
 
 class StackLevelChecker(ast.NodeVisitor):
     def __init__(self, file) -> None:
         self.file = file
-        self.violations = set()
+        self.deprecations = set()
 
     def _check_version(self, node: ast.Call) -> None:
         if node.func.id != "deprecated":
             return
         deprecated_version = Version(node.args[0].s)
         if BASE_CUR_VERSION >= deprecated_version:
-            self.violations.add(
-                f"{self.file}:{node.lineno}:{node.col_offset}: "
-                f"Found version '{deprecated_version}' in 'deprecated' should have been removed. "
-                f"Current version is '{CUR_VERSION}'."
+            msg = (
+                f"{RED}{self.file}:{node.lineno}:{node.col_offset}: "
+                f"Found version '{deprecated_version}' in 'deprecated' should have been removed.{RESET}"
             )
+            self.deprecations.add((True, msg))
+        else:
+            msg = (
+                f"{GREEN}{self.file}:{node.lineno}:{node.col_offset}: "
+                f"Found version '{deprecated_version}' in 'deprecated'.{RESET}"
+            )
+            self.deprecations.add((False, msg))
 
     def visit_Call(self, node: ast.Expr) -> None:
         if isinstance(node, ast.Call) and isinstance(node.func, ast.Name):
@@ -41,19 +48,24 @@ def check_file(file) -> int:
     stacklevel_checker = StackLevelChecker(file)
     stacklevel_checker.visit(tree)
 
-    for v in stacklevel_checker.violations:
-        print(v)
+    errs = 0
+    for err, msg in stacklevel_checker.deprecations:
+        if err:
+            errs += 1
+        print(msg)
 
-    return len(stacklevel_checker.violations)
+    return errs
 
 
 def main() -> None:
     files = check_output(["git", "ls-files", "."], cwd=PATH)
-    violations = 0
+    deprecations = 0
+    print(f"Current version is '{CUR_VERSION}'.")
     for file in files.decode().split("\n"):
         if file.endswith(".py"):
-            violations += check_file(file)
-    sys.exit(violations > 0)
+            deprecations += check_file(file)
+    sys.exit(deprecations > 0)
+
 
 if __name__ == "__main__":
     main()
