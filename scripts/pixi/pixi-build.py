b99d5647c7305beeebbe02001fe3ca5fca88ae74
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-14 14:43:50 +0200

    Print output as a table

--- scripts/pixi/pixi-build.py
+++ scripts/pixi/pixi-build.py
@@ -1,7 +1,9 @@
 import tarfile
 import zipfile
+from itertools import zip_longest
 
-from _artifact import download_files
+from _artifact import console, download_files
+from rich.table import Table
 
 
 def zip_files(zip_path):
@@ -34,39 +36,46 @@ def compare_tar_files(tar1_path, tar2_path):
     return tar1_missing, tar2_missing
 
 
+def generate_table(title, version1, version2, missing1, missing2):
+    table = Table(title=title)
+    table.add_column(f"[green]Files only in run 1\n{version1}[/green]", style="green")
+    table.add_column(f"[red]Files only in run 2\n{version2}[/red]", style="red")
+    for m1, m2 in zip_longest(missing1, missing2, fillvalue="-"):
+        table.add_row(m1, m2)
+    return table
+
+
+# 235 was before pixi PR
+repo = "holoviews"
 good_run, bad_run, good_path, bad_path = download_files(
-    "holoviews", 235, 246, "build.yaml", artifact_names=["pip", "conda"]
+    repo, 235, 246, "build.yaml", artifact_names=["pip", "conda"]
 )
 
 before_path = next(good_path.glob("*.whl"))
 after_path = next(bad_path.glob("*.whl"))
-missing_from_zip1, missing_from_zip2 = compare_zip_files(before_path, after_path)
+version1 = before_path.name.split("-py3")[0].split("-")[-1]
+version2 = after_path.name.split("-py3")[0].split("-")[-1]
 
-print("Files missing from main wheel:")
-print(missing_from_zip1)
-print("\nFiles missing from pixi wheel:")
-print(missing_from_zip2)
-print()
+missing_whl1, missing_whl2 = compare_zip_files(before_path, after_path)
+whl_table = generate_table(f"{repo} - wheels", version1, version2, missing_whl1, missing_whl2)
 
 before_path = next(good_path.glob("*.tar.gz"))
 after_path = next(bad_path.glob("*.tar.gz"))
-
-missing_from_tar1, missing_from_tar2 = compare_tar_files(before_path, after_path)
-
+missing_sdist1, missing_sdist2 = compare_tar_files(before_path, after_path)
 #  Filter out top-level example folder, this is a known bug
-missing_from_tar1 = [f for f in missing_from_tar1 if not f.startswith("$VERSION/examples")]
-missing_from_tar2 = [f for f in missing_from_tar2 if not f.startswith("$VERSION/examples")]
-
-print("Files missing from main sdist:")
-print(missing_from_tar1)
-print("\nFiles missing from pixi sdist:")
-print(missing_from_tar2)
-print()
+missing_sdist1 = [f for f in missing_sdist1 if not f.startswith("$VERSION/examples")]
+missing_sdist2 = [f for f in missing_sdist2 if not f.startswith("$VERSION/examples")]
+sdist_table = generate_table(
+    f"{repo} - sdist", version1, version2, missing_sdist1, missing_sdist2
+)
 
 before_path = next(good_path.glob("*.tar.bz2"))
 after_path = next(bad_path.glob("*.tar.bz2"))
+missing_conda1, missing_conda2 = compare_tar_files(before_path, after_path)
+conda_table = generate_table(
+    f"{repo} - conda", version1, version2, missing_conda1, missing_conda2
+)
 
-print("Files missing from main conda:")
-print(missing_from_tar1)
-print("\nFiles missing from pixi conda:")
-print(missing_from_tar2)
+console.print(whl_table)
+console.print(sdist_table)
+console.print(conda_table)
