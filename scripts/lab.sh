4ee2225bd700035a8af34a6bf72e101e7f72b739
Simon Høxbro Hansen <simon.hansen@me.com>
2024-04-19 11:28:25 +0200

    Update lab so it can search

--- scripts/lab.sh
+++ scripts/lab.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 if [[ $(jupyter server list 2>&1 | grep -o "localhost:8888" | wc -l) -eq 0 ]]; then
     BOKEH_RESOURCES=inline jupyter lab --port 8888 $HOLOVIZ_DEV  &>/tmp/jupyter_server.log & disown
 else
-    if [[ -n ${1-} ]]; then
-        file=$(fd ".ipynb" $HOLOVIZ_DEV -L | sed "s|$HOLOVIZ_DEV||" | fzf)
+    if [[ $# -gt 0 ]]; then
+        options=$(fd ".ipynb" $HOLOVIZ_DEV -L | sed "s|$HOLOVIZ_DEV||")
+        file=$(echo "$options" | fzf --select-1 --query "'$(echo "$@" | sed "s| | '|")")
         firefox "http://localhost:8888/lab/workspaces/auto-W/tree/$file"
     else
         firefox "http://localhost:8888/lab/workspaces/auto-W/"
