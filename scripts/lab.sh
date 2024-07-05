0409fb34b6c6d7569e100904f0858fa752a485b5
Simon Høxbro Hansen <simon.hansen@me.com>
2024-07-05 09:03:08 +0200

    Update fzf tmux

--- scripts/lab.sh
+++ scripts/lab.sh
@@ -7,7 +7,7 @@ if [[ $(jupyter server list 2>&1 | grep -o "localhost:8888" | wc -l) -eq 0 ]]; t
 else
     if [[ $# -gt 0 ]]; then
         options=$(fd ".ipynb" $HOLOVIZ_DEV -L | sed "s|$HOLOVIZ_DEV||")
-        file=$(echo "$options" | fzf-tmux -w --select-1 --query "'$(echo "$@" | sed "s| | '|")")
+        file=$(echo "$options" | fzf --tmux --select-1 --query "'$(echo "$@" | sed "s| | '|")")
         firefox "http://localhost:8888/lab/workspaces/auto-W/tree/$file"
     else
         firefox "http://localhost:8888/lab/workspaces/auto-W/"
