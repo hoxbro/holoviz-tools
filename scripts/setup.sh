75247199e441c33be918ca9ed423a599db8777cc
Simon Høxbro Hansen <simon.hansen@me.com>
2024-08-13 18:10:06 +0200

    Add hypothesis

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -29,7 +29,7 @@ ALL_PACKAGES=(
     ipywidgets_bokeh jupyter_bokeh
 
     # Testing
-    pytest pytest-xdist pytest-rerunfailures parameterized pytest-asyncio
+    pytest pytest-xdist pytest-rerunfailures parameterized pytest-asyncio hypothesis
     pytest-randomly detect-test-pollution nbval microsoft::pytest-playwright microsoft::playwright
 
     # Geo
