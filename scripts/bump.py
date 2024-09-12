3ec9aa8e0e74a0ca29150d1e590e811d30ce4ecc
Simon Høxbro Hansen <simon.hansen@me.com>
2024-09-12 19:28:14 +0200

    Allow empty commit

--- scripts/bump.py
+++ scripts/bump.py
@@ -87,7 +87,7 @@ def js_update(package: str, version: str):
         sys.exit(1)
 
     git("add", f"{package}/package.json", f"{package}/package-lock.json")
-    git("commit", "-m", f"Update {package}.js to {js_ver}", "--no-verify")
+    git("commit", "-m", f"Update {package}.js to {js_ver}", "--no-verify", "--allow-empty")
 
 
 def validate_version(package: str, version: str):
