9a5cb8a664b78f1ec0491be027dea3abaa4c09d9
Simon Høxbro Hansen <simon.hansen@me.com>
2024-09-24 09:16:31 +0200

    Update python-313.py

--- scripts/python-313.py
+++ scripts/python-313.py
@@ -5,17 +5,19 @@ GREEN, RED, RESET = "\033[0;32m", "\033[0;31m", "\033[0m"
 
 deps = [
     "fastparquet",
+    "h5py",
     "numba",
+    "numpy",
+    "pandas",
     "pyarrow",
-    "vtk",
-    "scikit-image",
     "python-duckdb",
     "pywin32",  # jupyter ecosystem
+    "scikit-image",
+    "vtk",
 ]
 
 url = "https://raw.githubusercontent.com/regro/cf-graph-countyfair/master/status/migration_json/python313.json"
-resp = httpx.get(url).raise_for_status()
-data = resp.json()
+data = httpx.get(url).raise_for_status().json()
 opts = ["awaiting-parents", "awaiting-pr", "bot-error", "done", "in-pr", "not-solvable"]
 
 for dep in sorted(deps):
