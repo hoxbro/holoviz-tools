2b699995c5b998dd4fbd2a7be8fda537f04ffc3d
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-03 11:07:49 +0100

    Changes to script

--- holoviz.sh
+++ holoviz.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-TOOLS="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd -P)/scripts"
+TOOLS="$(cd -- "$(dirname "$0")" &>/dev/null && pwd -P)/scripts"
+cli_py="$TOOLS/cli.py"
 
 ccd() {
     if [[ $PWD != $HOLOVIZ_DEV* && $PWD != $HOLOVIZ_REP* ]]; then
-        cd "$HOLOVIZ_DEV" || exit
+        cd "$HOLOVIZ_DEV" || exit 1
     fi
     if [[ $CONDA_DEFAULT_ENV != "holoviz" ]]; then
         # For activating conda environment
@@ -19,44 +20,45 @@ if [[ $1 == "lab" ]]; then
     shift
     bash "$TOOLS"/lab.sh "$@"
 elif [[ $1 == "save" ]]; then
-    RESULT=$("$TOOLS"/cli.py save.py)
+    RESULT=$($cli_py save.py)
     PANEL_SERVE_FILE=$(echo "$RESULT" | grep -E '^/home/')
     export PANEL_SERVE_FILE
 elif [[ $1 == "setup" ]]; then
     bash "$TOOLS"/setup.sh
 elif [[ $1 == "clean" ]]; then
     ccd
-    "$TOOLS"/cli.py cleanup.py
+    $cli_py cleanup.py
 elif [[ $1 == "action-status" ]]; then
-    "$TOOLS"/cli.py action_status.py
+    $cli_py action_status.py
 elif [[ $1 == "version-finder" ]]; then
-    "$TOOLS"/cli.py version_finder.py
+    $cli_py version_finder.py
 elif [[ $1 == "artifact-test" ]]; then
     shift
-    "$TOOLS"/cli.py pixi/artifact.py "$@"
+    $cli_py pixi/artifact.py "$@"
 elif [[ $1 == "artifact-build" ]]; then
     shift
-    "$TOOLS"/cli.py build.py "$@"
+    $cli_py build.py "$@"
 elif [[ $1 == "changelog" ]]; then
     shift
-    "$TOOLS"/cli.py changelog.py "$@"
+    $cli_py changelog.py "$@"
 elif [[ $1 == "deprecate" ]]; then
     shift
-    "$TOOLS"/cli.py deprecate.py "$@"
+    $cli_py deprecate.py "$@"
 elif [[ $1 == "serve" ]]; then
     shift
     source "$TOOLS"/serve.sh "$@"
 elif [[ $1 == "pixi-lock" ]]; then
     shift
-    "$TOOLS"/cli.py pixi/lock.py "$@"
+    $cli_py pixi/lock.py "$@"
 elif [[ $1 == "workspace" ]]; then
     shift
     bash "$TOOLS"/workspace/"$1".sh "$@"
 elif [[ $1 == "bump" ]]; then
-    "$TOOLS"/cli.py bump.py "$@"
+    $cli_py bump.py "$@"
 elif [[ $1 == "python-3.13" ]]; then
-    "$TOOLS"/cli.py python-313.py
+    $cli_py python-313.py
 elif [[ $1 ]]; then
+    printf "\033[0;31mholoviz %s is an invalid command\033[0m\n" "$1"
     (exit 1)
 else
     ccd
