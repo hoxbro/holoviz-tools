162dc75bf7c5d9e0d8b235da3b905d1072b117d5
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-12 13:51:21 +0100

    Update serve

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -6,20 +6,19 @@ if [ -n "$1" ]; then
 fi
 
 if [ -z "$PANEL_SERVE_FILE" ]; then
-    UNSORTED_FILES=$(fd --regex '\d{3,5}.+?\.py$' -t f $DIR --print0)
-    SORTED_FILES=$(echo $UNSORTED_FILES | xargs -0 stat -c "%Y %N" 2>/dev/null | sort -r | awk '{print $2}' | tr -d "'" | grep -v 'archive' | sed "s?$DIR??" | grep -e '^dev_')
+    SORTED_FILES=$(fd --regex '\d{3,5}.+?\.py$' -t f "$DIR" -X stat -c "%Y %N" 2>/dev/null | sort -r | awk '{print $2}' | tr -d "'" | grep -v 'archive' | sed "s?$DIR??" | grep '^dev_')
     if [ -z "$1" ]; then
-        FILE=$(echo $SORTED_FILES | fzf --preview "bat $DIR{} --color always")
+        FILE=$(echo "$SORTED_FILES" | fzf --preview "bat $DIR{} --color always") || return 1
     else
-        FILE=$(echo $SORTED_FILES | fzf --preview "bat $DIR{} --color always" --select-1 --query $1)
+        FILE=$(echo "$SORTED_FILES" | fzf --preview "bat $DIR{} --color always" --select-1 --query "$1") || return 1
     fi
     export PANEL_SERVE_FILE=$DIR$FILE
 fi
 
 PORT_USED=$(lsof -t -i :5006)
 if [ -n "$PORT_USED" ]; then
-    kill $PORT_USED
+    kill "$PORT_USED"
     echo "$(date +'%Y-%m-%d %H.%M.%S.%3N') Killed running panel serve on port 5006"
 fi
 
-panel serve $PANEL_SERVE_FILE --autoreload
+panel serve "$PANEL_SERVE_FILE" --autoreload
