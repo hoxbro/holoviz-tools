dc501d1a035be994123c671aeb284fd1e8d9df06
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-10-28 18:51:00 +0100

    Update bokeh setup script

--- scripts/bokeh/setup.sh
+++ scripts/bokeh/setup.sh
@@ -10,11 +10,13 @@ source "$CONDA_HOME/etc/profile.d/conda.sh"
 conda activate base
 
 conda env remove -n bkdev -y >/dev/null || true
-mamba env create -n bkdev -f conda/environment-test-3.11.yml -y
-conda uninstall --force --offline -y pre-commit || true
+mamba env create -n bkdev -f conda/environment-test-3.13.yml -y
 conda env config vars set BOKEH_RESOURCES=server-dev -n bkdev
 conda env config vars set BOKEH_CHROME=~/.local/bin/chrome-dev -n bkdev
 conda activate bkdev
+
+conda uninstall --force --offline -y pre-commit || true
+python -m pip uninstall ruff -y || true
 
 (cd bokehjs && npm ci)
 (cd bokehjs && node make dev)
